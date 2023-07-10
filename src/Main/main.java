680033ab69bff1c68a73e34ad7dab4b453e653b0
ahmed_habib <ahmedhabib0660@gmail.com>
2023-07-10 12:33:44 +0300

    e

--- src/Main/main.java
+++ src/Main/main.java
@@ -14,7 +14,18 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code  logic here
+        int[] list = {1, 2, -5, 3, -8, 9};
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < (list.length - i); j++) {
+                if (j + 1 != list.length && list[j] > list[j + 1]) {
+                    int temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                }
+            }
+        }
+        for (int i : list) {
+            System.out.println(i);
+        }
     }
-    
 }
