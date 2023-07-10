d5fcf026d34b255f9aedcf72e81200f220ae784a
ahmed_habib <ahmedhabib0660@gmail.com>
2023-07-10 15:25:24 +0300

    Update ShellSort.java

--- src/Main/ShellSort.java
+++ src/Main/ShellSort.java
@@ -9,5 +9,32 @@ package Main;
  * @author Ahmed
  */
 public class ShellSort {
+
+    public static void sortASC(int[] list) {
+        for (int gap = list.length / 2; gap > 0; gap /= 2) {
+
+            for (int i = gap; i < list.length; i++) {
+                int temp = list[i];
+                int j = i;
+                for (; j >= gap && list[j - gap] > temp; j -= gap) {
+                    list[j] = list[j - gap];
+                }
+                list[j] = temp;
+            }
+        }
+    }
     
+    public static void sortDSC(int[] list) {
+        for (int gap = list.length / 2; gap > 0; gap /= 2) {
+
+            for (int i = gap; i < list.length; i++) {
+                int temp = list[i];
+                int j = i;
+                for (; j >= gap && list[j - gap] < temp; j -= gap) {
+                    list[j] = list[j - gap];
+                }
+                list[j] = temp;
+            }
+        }
+    }
 }
