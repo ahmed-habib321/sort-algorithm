9ff2ea2a59909f69f132977ea06b9c74faf44d64
ahmed_habib <ahmedhabib0660@gmail.com>
2023-07-10 13:07:19 +0300

    bubble sort done

--- src/Main/BubbleSort.java
+++ src/Main/BubbleSort.java
@@ -9,10 +9,52 @@ package Main;
  * @author Ahmed
  */
 public class BubbleSort {
-    
-    public void sort(int[] list){
-        
-        
+
+    public static void sortASC(int[] list) {
+        for (int i = list.length-1; i > 0 ; i--) {
+            for (int j = 0; j < i; j++) {
+                
+                if (list[j] > list[j + 1]) {
+                    swap(list, j, j+1);
+                }
+                //trace(j,list);
+                
+            }
+        }
     }
     
+    public static void sortDSC(int[] list) {
+        for (int i = list.length-1; i > 0 ; i--) {
+            for (int j = 0; j < i; j++) {
+                
+                if (list[j] < list[j + 1]) {
+                    swap(list, j, j+1);
+                }
+                //trace(j,list);
+            }
+        }
+    }
+    
+
+    public static void swap(int[] array, int i, int j) {
+        if (i != j) {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+    
+    public static void trace(int position, int[] array){
+        String currentstate = "Array =[";
+        for (int i : array) {
+            
+            currentstate += i+"";
+            if (array[array.length-1]!=i) {
+            currentstate += ",";    
+            }
+        }
+        currentstate +="] , position = " + position;
+        System.out.println(currentstate);
+    }
+
 }
--- src/Main/main.java
+++ src/Main/main.java
@@ -15,15 +15,9 @@ public class main {
      */
     public static void main(String[] args) {
         int[] list = {1, 2, -5, 3, -8, 9};
-        for (int i = 0; i < list.length; i++) {
-            for (int j = 0; j < (list.length - i); j++) {
-                if (j + 1 != list.length && list[j] > list[j + 1]) {
-                    int temp = list[j];
-                    list[j] = list[j + 1];
-                    list[j + 1] = temp;
-                }
-            }
-        }
+        
+        BubbleSort.sortASC(list);
+        
         for (int i : list) {
             System.out.println(i);
         }
